758d1c39b403544b19496a24043d1995d9c9d8d4
Varun Kohli <kohli.varun5@gmail.com>
2018-03-30 11:54:13 -0400

    .

--- src/index.ml
+++ src/index.ml
@@ -1,10 +1,31 @@
+let credentials = Coinbase.credentials 
+                      ~apiKey:""
+                      ~apiSecret:""
+let client = Coinbase.client credentials 
+
+
 let port = 3000
 let hostname = "127.0.0.1"
 let create_server http =
   let server = http##createServer begin fun [@bs] _ resp ->
-      resp##statusCode #= 200;
-      resp##setHeader "Content-Type" "text/plain";
-      resp##_end "Hello world\n"
+      Coinbase.ExchangeRates.get 
+        client 
+        (Coinbase.ExchangeRates.req ~currency:"BTC")
+        (fun _ rates -> 
+    
+          let entries = Js.Dict.entries rates##data##rates in 
+          Js.log(entries);
+          let res = 
+            Array.fold_left 
+              (fun res (key,value) -> 
+                  res ^ Printf.sprintf "\n %s : %s" key value )
+              "" entries 
+          in
+          resp##statusCode #= 200;
+          resp##setHeader "Content-Type" "text/plain";
+          resp##_end res 
+          );
+      ()
     end
   in
   server##listen port hostname begin fun [@bs] () ->
